459df0e9035fe34c26541533b288d88897c7853b
Hosam Ashraf <hosamhsm98@outlook.com>
2020-12-13 12:05:11 +0530

    code split, gallery update

--- pages/gallery/index.js
+++ pages/gallery/index.js
@@ -1,24 +1,20 @@
 import { Container } from 'react-bootstrap';
+import Heading from '../../components/Heading';
 import MasonryView from '../../components/MasonryView';
-import SlideShow from '../../components/SlideShow';
+import styles from "./gallery.module.scss";
+
 function Gallery() {
   return (
-    <>
-      <SlideShow/>
-    <Container>
-      <div className="headerContainer">
-        <h6 className="headerTitle">MURALS & WALL ARTS</h6>
-          <span>Murals create a tangible sense of place, destination,
+    <Container className={styles.container}>
+        <Heading title="MURALS & WALL ARTS" text="Murals create a tangible sense of place, destination,
           resulting in increased foot traffic while adding color, vibrancy and character
           to urban environment. Murals contrast the negative mental health effects of concrete
           and asphalt and can have therapeutic benefits for mentallyill populations. Our distinguishing
           characteristic of mural and wall arts is that the architectural elements of given space are
           harmoniously incorporated in to picture. We provide a soothing optimism in your residing space
-          with our artistic touch.</span>
-      </div>
+          with our artistic touch."/>
       <MasonryView />
       </Container>
-    </>
   );
 }
 
