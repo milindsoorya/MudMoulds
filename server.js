5282cfe1362f26cc7577dc897b90b4f133805bc9
sarangr <sarangr@qburst.com>
2020-12-18 11:41:59 +0530

    Added admin page

--- server.js
+++ server.js
@@ -1,26 +1,32 @@
 const express = require('express')
 const next = require('next')
-
 const dev = process.env.NODE_ENV !== 'production'
 const app = next({ dev })
 const handle = app.getRequestHandler()
-
 const adminRoute = require('./admin/index')
-    
+const mongoose = require('mongoose')
+
+// Need to change during production
+const db =  mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
 app.prepare()
-.then(() => {
-  const server = express();
-  server.use('/admin',adminRoute);
-  server.get('*', (req, res) => {
-    return handle(req, res)
+  .then(() => {
+    const server = express();
+
+    server.use('/admin', adminRoute);
+    server.get('*', (req, res) => {
+      return handle(req, res)
+    })
+
+    server.listen(3000, (err) => {
+      if (err) throw err
+      console.log('> Ready on http://localhost:3000')
+    })
   })
-  
-  server.listen(3000, (err) => {
-    if (err) throw err
-    console.log('> Ready on http://localhost:3000')
+  .catch((ex) => {
+    console.error(ex.stack)
+    process.exit(1)
   })
-})
-.catch((ex) => {
-  console.error(ex.stack)
-  process.exit(1)
-})
